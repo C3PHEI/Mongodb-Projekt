aa8dea484fcf2326026d0791e5e24ce30fb7196a
Maximilian Läpple <m.max32@yahoo.com>
2024-01-30 21:23:54 +0100

    Projektdateien hinzufügen.

---
+++ Mangodb/Controllers/KlasseController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using MongoExample.Services;
+using MongoExample.Models;
+
+namespace MangoExample.Controllers
+{
+    [Controller]
+    [Route("api/[controller]")]
+    public class KlasseController : Controller
+    {
+        private readonly MongoDBService _mongoDBService;
+
+        public KlasseController(MongoDBService mongoDBService)
+        {
+            _mongoDBService = mongoDBService;
+        }
+
+        [HttpGet]
+        public async Task<List<Bestellungen>> Get() 
+        { 
+            return await _mongoDBService.GetAsync();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Bestellungen bestellungen) { 
+            await _mongoDBService.CreateAsync(bestellungen);
+            return CreatedAtAction(nameof(Get),new { id = bestellungen.Id }, bestellungen);
+        }
+        //noch anpassen
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> AddToKlasse(string id, [FromBody] string Klassenlehrer) 
+        {
+            await _mongoDBService.AddToKlasseAsync(id, Klassenlehrer);
+            return NoContent();
+        }
+        //noch anpassen
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id) 
+        {
+            await _mongoDBService.DeteleAsync(id);
+            return NoContent();
+        }
+        // noch anpassen
+    }
+}
---
+++ Mangodb/Models/Bestellungen.cs
@@ -0,0 +1,49 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace MongoExample.Models
+{
+    public class Bestellungen
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("BestellungsID")]
+        public int BestellungsID { get; set; }
+
+        [BsonElement("Prioritaet")]
+        public string? Prioritaet { get; set; }
+
+        [BsonElement("Service")]
+        public string? Service { get; set; }
+
+        [BsonElement("Name")]
+        public string? Name { get; set; }
+
+        [BsonElement("Email")]
+        public string? Email { get; set; }
+
+        [BsonElement("Telefon")]
+        public string? Telefon { get; set; }
+
+        [BsonElement("DatumEinreichung")]
+        public DateTime DatumEinreichung { get; set; }
+
+        [BsonElement("DatumBisFertig")]
+        public DateTime DatumBisFertig { get; set; }
+
+        [BsonElement("Preis")]
+        public double Preis { get; set; }
+
+        [BsonElement("StatusName")]
+        public string? StatusName { get; set; }
+
+        [BsonElement("MitarbeiterName")]
+        public string? MitarbeiterName { get; set; }
+    }
+}
+
+
